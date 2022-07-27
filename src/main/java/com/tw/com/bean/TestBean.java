5c81e1048bfcc31d7e4c05a32dab76ee4ecb3c64
aeifkz <devilmaycry0616@hotmail.com>
2022-07-27 17:06:22 +0800

    init project

---
+++ src/main/java/com/tw/com/bean/TestBean.java
@@ -0,0 +1,24 @@
+package com.tw.com.bean;
+
+public class TestBean {
+
+    private String name;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
